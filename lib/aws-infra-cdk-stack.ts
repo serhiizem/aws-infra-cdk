4617f155a9e6b98410c4efbe48fba5e0f65882dd
Serhii Zemlianyi <serhiizem@gmail.com>
2023-12-30 12:12:37 +0200

    networking construct representing VPC with public and private subnets

--- lib/aws-infra-cdk-stack.ts
+++ lib/aws-infra-cdk-stack.ts
@@ -1,6 +1,7 @@
 import {CfnOutput, Stack, StackProps} from 'aws-cdk-lib';
 import {Construct} from 'constructs';
 import {Bucket, BucketEncryption} from "aws-cdk-lib/aws-s3";
+import {Networking} from "./networking";
 
 export class AwsInfraCdkStack extends Stack {
     constructor(scope: Construct, id: string, props?: StackProps) {
@@ -13,6 +14,10 @@ export class AwsInfraCdkStack extends Stack {
         new CfnOutput(this, "DocumentsBucketNameExport", {
             value: bucket.bucketName,
             exportName: "DocumentsBucketName"
+        })
+
+        new Networking(this, "NetworkingConstruct", {
+            maxAzs: 2
         })
     }
 }
---
+++ lib/networking.ts
@@ -0,0 +1,33 @@
+import {Construct} from "constructs";
+import {IpAddresses, IVpc, SubnetType, Vpc} from "aws-cdk-lib/aws-ec2";
+
+interface NetworkingProps {
+    maxAzs: number
+}
+
+export class Networking extends Construct {
+
+    public readonly vpc: IVpc
+
+    constructor(scope: Construct, id: string, props: NetworkingProps) {
+        super(scope, id);
+
+        this.vpc = new Vpc(this, 'AppVPC', {
+            ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
+            maxAzs: props.maxAzs,
+            subnetConfiguration: [
+                {
+                    subnetType: SubnetType.PUBLIC,
+                    name: 'Public',
+                    cidrMask: 24,
+                },
+                {
+                    cidrMask: 24,
+                    name: 'Private',
+                    subnetType: SubnetType.PRIVATE_ISOLATED,
+                }
+            ],
+        });
+
+    }
+}
