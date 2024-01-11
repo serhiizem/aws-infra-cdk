49658d0db9f2919ff6a1d1eae9ec51c33673d369
Serhii Zemlianyi <serhiizem@gmail.com>
2024-01-11 20:41:02 +0200

    use NodeJsFunction for esbuild bundling

--- lib/api.ts
+++ lib/api.ts
@@ -1,16 +1,17 @@
 import {Construct} from "constructs";
-import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
+import {Runtime} from "aws-cdk-lib/aws-lambda";
 import path from "path";
+import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class DocumentManagementAPI extends Construct {
 
     constructor(scope: Construct, id: string) {
         super(scope, id);
 
-        new Function(this, 'MyGetDocumentsFunction', {
+        new NodejsFunction(this, 'MyGetDocumentsFunction', {
             runtime: Runtime.NODEJS_18_X,
             handler: 'getDocuments',
-            code: Code.fromAsset(path.join(__dirname, '..', 'api', 'getDocuments'))
+            entry: path.join(__dirname, '..', 'api', 'getDocuments', 'index.ts')
         });
     }
 }
